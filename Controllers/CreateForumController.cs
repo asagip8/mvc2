a095c2472b6676d10a48783d4a46077c26302fed
sagi <asagip8@hotmail.com>
2015-06-02 15:01:32 +0300

    s
    s

---
+++ Controllers/CreateForumController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+using ForumApplication.Models;
+using System.IO;
+
+namespace ForumApplication.Controllers
+{
+    public class CreateForumController : Controller
+    {
+        public ActionResult CreateForum()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateNewForum()
+        {
+            string title = Request["Forumtitle"].ToString();
+            string admin = Request["adminUserName"].ToString();
+            List<string> admins = new List<string>();
+            admins.Add(admin);
+            ForumSystem fs = ForumSystem.initForumSystem();
+            Forum newForum = new Forum(title, admins);
+            fs.createForum(newForum);
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/ForumExampleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForumApplication.Controllers
+{
+    public class ForumExampleController : Controller
+    {
+        //
+        // GET: /ForumExample/
+
+        public ActionResult ForumExample()
+        {
+            return View();
+        }
+
+    }
+}
